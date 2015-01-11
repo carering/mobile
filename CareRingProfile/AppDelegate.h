f1d3b3834ec782c266d924619c6eda5d4239005a
James Stiehl <jstiehl@gs-mbp15-james-2.local>
2015-01-10 21:17:21 -0800

    Initial Commit

---
+++ CareRingProfile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CareRingProfile
+//
+//  Created by James Stiehl on 1/10/15.
+//  Copyright (c) 2015 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CareRingProfile/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  CareRingProfile
+//
+//  Created by James Stiehl on 1/10/15.
+//  Copyright (c) 2015 James Stiehl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
